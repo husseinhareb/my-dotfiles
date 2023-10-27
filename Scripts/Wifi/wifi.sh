77a8a6e10088e93cbcbef0997cb6b2d7f8b6b355
husseinhareb <shtam.hareb12@gmail.com>
2023-10-27 21:41:47 +0300

    trying

--- Scripts/Wifi/wifi.sh
+++ Scripts/Wifi/wifi.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-# Check if iftop is installed
-if ! command -v iftop &>/dev/null; then
-    echo "iftop is not installed. You can install it using your package manager (e.g., 'apt-get install iftop' on Debian/Ubuntu)."
-    exit 1
-fi
-
-# Run iftop and capture the output
-sudo iftop -t -s 1 -n -N 2> /dev/null | awk '/Total send rate:/ { upload_speed = $6; } /Total receive rate:/ { download_speed = $6; } END { print "Upload Speed: " upload_speed " / Download Speed: " download_speed; }'
+printf '%s\nTIME (s)\tSIGNAL STRENGTH (dBm)\tBITRATE (MBit/s)\n' "$(date --iso-8601=seconds)" >>log
+for ((i=0; i<=60; i=i+5)); do
+    iw dev wlp0s20f3 station dump | awk -vt=$i '$1=="signal:"{s=$2} $2=="bitrate:"{b=$3} END {printf "%d\t%d\t%.1f\n", t, s, b}' >>log
+    sleep 5
+done
