b4e5546382fbcca3381796c0868fc9a11e110225
husseinhareb <shtam.hareb12@gmail.com>
2023-11-01 20:29:56 +0200

    calendar

---
+++ eww/spotify/calendar/calendar_widget.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Files and CMD
+FILE="$HOME/.cache/eww_launch.calendar"
+CFG="$HOME/.config/eww/calendar/"
+EWW=`which eww`
+
+## Run eww daemon if not running already
+if [[ ! `pidof eww` ]]; then
+	${EWW} daemon
+	sleep 1
+fi
+
